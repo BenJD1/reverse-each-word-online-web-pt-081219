1c0d382de19a41045ae5db109bf1d1d65f659c51
Ben Dunnell <bjdunnell96@gmail.com>
2019-09-09 21:33:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,17 @@
+require "pry"
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   array = sentence.split
+#   array.each do |s|
+#     new_sentence << s.reverse
+#   end
+#   new_sentence.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  sentence.split(" ").collect  {|word| word.reverse}
-  .join(" ")
-end
+  array = sentence.split
+    array = array.collect do |s|
+      s.reverse!
+    end
+    array.join(" ")
+  end
